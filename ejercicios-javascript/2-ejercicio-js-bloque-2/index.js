468e3b82f3cd4a26f41bcfc1bb53af75a2f0a415
Mariangelica Rodriguez <gromarant@gmail.com>
2022-09-05 12:42:23 +0200

    Fix exercise, add data of the book

--- ejercicios-javascript/2-ejercicio-js-bloque-2/index.js
+++ ejercicios-javascript/2-ejercicio-js-bloque-2/index.js
@@ -1,3 +1,13 @@
-const datosPersonales = [ "Mariangelica Rodríguez", 31, true, "2 de marzo", "idea producto y negocio"];
+const datosPersonales = [ 
+    "Mariangelica Rodríguez", 
+    31, 
+    true, 
+    "2 de marzo", 
+{ 
+    titulo: "idea producto y negocio",
+    autor: "Justo Hidalgo ",
+    fecha: new Date(2017, 04, 1),
+    url: "https://www.amazon.es/Idea-Producto-Negocio-innovadores-Emprendedores/dp/8494606263"
+  },];
 
 console.log(datosPersonales);
