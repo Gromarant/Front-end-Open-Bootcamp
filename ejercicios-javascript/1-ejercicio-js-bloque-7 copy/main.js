6c6827cf10244eeeca6d1b9eb29793646b9bb231
Mariangelica Rodriguez <gromarant@gmail.com>
2022-09-24 00:28:09 +0200

    Make seventh javascript exercise of OpenBootcamp course

---
+++ ejercicios-javascript/1-ejercicio-js-bloque-7 copy/main.js
@@ -0,0 +1,9 @@
+// Crea un archivo llamado conjuntos.js que contenga las siguientes líneas
+// 1. Un nuevo Set con los nombres de tu familia
+    const family = new Set([ 'Miguel', 'Luis', 'Victoria', 'Mariangelica', 'Carolina', 'Jose']);
+
+// 2. Modifica el Set original añadiendo tu nombre (duplicado) (debería darte lo mismo)
+    family.add('Mariangelica');
+
+// 3. Modifica el Set original añadiendo el nombre "Javascript" (ya que empieza a formar parte de tu vida ;)
+    family.add('Javascript');
