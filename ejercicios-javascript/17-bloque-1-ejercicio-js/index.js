6f9c4a4f7e79e9b79b592bc9ccde027e8a210d5f
Mariangelica Rodriguez <gromarant@gmail.com>
2022-10-17 13:17:52 +0200

    Make first javascript exercise of the seventh module

---
+++ ejercicios-javascript/17-bloque-1-ejercicio-js/index.js
@@ -0,0 +1,40 @@
+function initMap() {
+  const tajoToledo = { lat: 39.869070, lng: -4.013169 };
+  
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: tajoToledo,
+  });
+
+  const infoWindow = new google.maps.InfoWindow({
+    content: "",
+    disableAutoPan: true,
+  });
+  
+  const labels = `${/A-Z/}`;
+  console.log( labels);
+  const markers = locations.map(( position, i ) => {
+    const label = labels[ i % labels.length ];
+    const marker = new google.maps.Marker({
+      position,
+      map,
+    });
+    
+    marker.addListener("click", () => {
+      infoWindow.setContent(label);
+      infoWindow.open(map, marker);
+    });
+    return marker;
+  });
+
+  new MarkerClusterer({ markers, map });
+  
+};
+
+const locations = [
+  { lat: 39.869070, lng: -4.013169 },
+  { lat: 38.166373644241126, lng: -0.47432421876704955 },
+  { lat: 40.4501353068216, lng: -3.6981600517698245 },
+];
+
+window.initMap = initMap;
